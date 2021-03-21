b9f67d59568a91f12149d7b72fec546a01a71f57
Ethan Lo <48305607+ethanlo100@users.noreply.github.com>
2021-03-21 14:08:36 -0700

    added idle/walk anim

---
+++ Dungeon Diver/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    // Update is not a good place to use for physics as framerate constantly changes
+    // use fixed Update function (shown after the update function)
+    void Update()
+    {
+        //input for movement
+
+        //gives us a number for direction 
+        movement.x = Input.GetAxisRaw("Horizontal");    //(i.e. pressing right arrow returns 1)
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    //by default this function is called 50 times a second
+    private void FixedUpdate()
+    {
+        //actually doing movement
+
+        //multiplying by time.fixeddeltatime allows for constant movespeed
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+
+        //makes the player sprite aim at the mouse
+        //however, we don't want the sprite to actually aim.
+        //rb.rotation = angle;  
+    }
+}
