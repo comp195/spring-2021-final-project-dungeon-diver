5d6f957176e8f9bf41c2f971213275d23a9a1202
Ethan Lo <48305607+ethanlo100@users.noreply.github.com>
2021-03-31 07:16:39 -0700

    aiming complete, need to fix code for map gen

---
+++ Dungeon Diver/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public Transform target;
+    float maxScreenPoint = 0.2f;
+    private Vector3 velocity;
+    private float dampTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        
+        Vector3 mousePos = Input.mousePosition * maxScreenPoint + new Vector3(Screen.width, Screen.height/1.2f, 0f) * ((1f - maxScreenPoint) * 0.5f);
+        //Vector3 position = (target.position + GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition)) / 2f;
+        Vector3 position = (target.position + GetComponent<Camera>().ScreenToWorldPoint(mousePos)) / 2f;
+        Vector3 destination = new Vector3(position.x, position.y, -10);
+        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+    }
+}
--- Dungeon Diver/Assets/Scripts/roomSpawner.cs
+++ Dungeon Diver/Assets/Scripts/roomSpawner.cs
@@ -18,17 +18,18 @@ public class roomSpawner : MonoBehaviour
 
     public bool spawned = false;
     public int numRooms = 5;
+    public int index = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<roomTemplates>();
-        Invoke("Spawn", 0.5f);             // call the Spawn() function after 0.1 seconds
+        Invoke("Spawn", 0.2f);             // call the Spawn() function after 0.1 seconds
     }
 
     void Spawn()
     {
-        if (spawned == false)             // make sure that the rooms are not already spawned in
+        if (spawned == false && index != numRooms)             // make sure that the rooms are not already spawned in
         {
             if (openingDirection == 1)          // spawn room with bottom door
             {
@@ -40,17 +41,19 @@ public class roomSpawner : MonoBehaviour
                 rand = Random.Range(0, roomArrLength);
                 Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
             }
-            else if (openingDirection == 3)     // spawn room with left door
-            {
-                rand = Random.Range(0, roomArrLength);
-                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
-            }
-            else if (openingDirection == 4)     // spawn room with right door
-            {
-                rand = Random.Range(0, roomArrLength);
-                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
-            }
+            //else if (openingDirection == 3)     // spawn room with left door
+            //{
+            //    rand = Random.Range(0, roomArrLength);
+            //    Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            //}
+            //else if (openingDirection == 4)     // spawn room with right door
+            //{
+            //    rand = Random.Range(0, roomArrLength);
+            //    Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            //}
             spawned = true;
+            index++;
+            print(index);
         }
 
         
@@ -64,9 +67,9 @@ public class roomSpawner : MonoBehaviour
             if (collision.GetComponent<roomSpawner>().spawned == false && spawned == false)
             {
                 // spawn walls blocking off any openings
-
+                Instantiate(templates.closeRooms, transform.position, Quaternion.identity);
             }
-            Destroy(gameObject);
+            spawned = true;
         }
     }
 }
