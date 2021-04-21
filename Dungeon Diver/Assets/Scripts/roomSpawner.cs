070de0f91366a9c306d31753b6685f15587fe570
Ethan Lo <48305607+ethanlo100@users.noreply.github.com>
2021-04-21 08:24:57 -0700

    still trying to fix

--- Dungeon Diver/Assets/Scripts/roomSpawner.cs
+++ Dungeon Diver/Assets/Scripts/roomSpawner.cs
@@ -15,16 +15,16 @@ public class roomSpawner : MonoBehaviour
     private roomTemplates templates;
     private int rand;
     private int roomArrLength = 8;
+    public float waitTime = 4f;
 
     private bool spawned = false;
-    public int numRooms = 5;
-    public int index = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        Destroy(gameObject, waitTime);
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<roomTemplates>();
-        Invoke("Spawn", 0.2f);             // call the Spawn() function after 0.1 seconds
+        Invoke("Spawn", 0.1f);             // call the Spawn() function after 0.1 seconds
     }
 
     void Spawn()
@@ -37,39 +37,38 @@ public class roomSpawner : MonoBehaviour
                 Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
                 
             }
-            if (openingDirection == 2)     // spawn room with top door
+            else if (openingDirection == 2)     // spawn room with top door
             {
                 rand = Random.Range(0, roomArrLength);
                 Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
             }
-            if (openingDirection == 3)     // spawn room with left door
+            else if (openingDirection == 3)     // spawn room with left door
             {
                 rand = Random.Range(0, roomArrLength);
                 Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
             }
-            if (openingDirection == 4)     // spawn room with left door
-            {
-                rand = Random.Range(0, roomArrLength);
-                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
-            }
+            //else if (openingDirection == 4)     // spawn room with left door
+            //{
+            //    rand = Random.Range(0, roomArrLength);
+            //    Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            //}
             spawned = true;
-            index++;
-            print(index);
         }
-
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.CompareTag("spawnPoint"))         // so that rooms don't spawn on top of each other
+        if (other.CompareTag("spawnPoint"))         // so that rooms don't spawn on top of each other
         {
-            if (collision.GetComponent<roomSpawner>().spawned == false && spawned == false)
+            if (other.GetComponent<roomSpawner>().spawned == false && spawned == false)
             {
                 // spawn walls blocking off any openings
-                Instantiate(templates.closeRooms, transform.position, Quaternion.identity);
+                //Instantiate(templates.closeRooms, transform.position, Quaternion.identity);
+                Destroy(gameObject);
             }
             spawned = true;
+            print("OUCH");
         }
     }
 }
